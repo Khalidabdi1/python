d9ae140bef39277b84b6f2eb86c9372b4c463d7f
Khalid Abdi <xamza2009@gmail.com>
2025-11-14 18:17:03 +0300

    learn how to change data tyep

--- test.py
+++ test.py
@@ -1,27 +1,60 @@
-def name(name):
-    return name
+# def name(name):
+#     return name
 
 
-x=name("khalid")
-print(x)
+# x=name("khalid")
+# print(x)
 
 
 
-num1=input("enter first number : ")
+# num1=input("enter first number : ")
 
-num2=input("enter second number : ")
+# num2=input("enter second number : ")
 
-def sub(num1,num2):
-    return int(num1) - int(num2)
+# def sub(num1,num2):
+#     return int(num1) - int(num2)
 
 
-print(sub(num1,num2))
-print(type(sub))
+# print(sub(num1,num2))
+# print(type(sub))
 
-for chars in "khalid":
-    print(chars)
 
-for i in range(100):
-    print(i)
+
+# x=["2",2,True,2.4]
+
+# print(len(x))
+# for i in x:
+#     print()
+
+
+# if 5>10:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# type change 
+
+x=30000
+
+print(type(str(x)))
+
+print('''" thue " one''')
+
+
+pi=3.14
+massage="hi khalid"
+n=17
+
+
+user_name="dwa"
+
+print(user_name)
+
+
+
+
+
+
 
 
