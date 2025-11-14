bbcb5b44436b92ce07ee5eb5809879b3d688afc1
Khalid Abdi <xamza2009@gmail.com>
2025-11-14 22:51:21 +0300

    try turtle with python

--- test.py
+++ test.py
@@ -1,60 +1,18 @@
-# def name(name):
-#     return name
+import turtle ;
 
+wn=turtle.Screen()
+khalid=turtle.Turtle()
+khalid.forward(100)
+khalid.left(90)
+khalid.forward(100)
 
-# x=name("khalid")
-# print(x)
+khalid.left(90)
+khalid.forward(100)
 
-
-
-# num1=input("enter first number : ")
-
-# num2=input("enter second number : ")
-
-# def sub(num1,num2):
-#     return int(num1) - int(num2)
-
-
-# print(sub(num1,num2))
-# print(type(sub))
-
-
-
-# x=["2",2,True,2.4]
-
-# print(len(x))
-# for i in x:
-#     print()
-
-
-# if 5>10:
-#     print("yes")
-# else:
-#     print("no")
-
-
-# type change 
-
-x=30000
-
-print(type(str(x)))
-
-print('''" thue " one''')
-
-
-pi=3.14
-massage="hi khalid"
-n=17
-
-
-user_name="dwa"
-
-print(user_name)
+khalid.left(90)
+khalid.forward(100)
 
 
 
 
-
-
-
-
+turtle.done()
